96f29a2eb4b44a5cd4a436aa40bb3e39711e9ca4
aljoharah4 <jojoas2004@gmail.com>
2025-07-21 23:05:05 +0300

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My Graduation Project',
       home: Scaffold(
-        appBar: AppBar(title: Text('Welcome')),
-        body: Center(child: Text('Hello Firebase!')),
+        appBar: AppBar(title: const Text("Firebase Test")),
+        body: const Center(child: Text("Firebase initialized successfully!")),
       ),
     );
   }
 }
-
 /*
 void main() {
   runApp(const MyApp());
