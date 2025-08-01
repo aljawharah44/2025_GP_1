14bff9977454ef71770c2e20bf753e9d808ce2bb
aljoharah4 <jojoas2004@gmail.com>
2025-08-01 03:26:47 +0300

    jj

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,6 @@ class MunirApp extends StatelessWidget {
 
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'screens/splash_screen.dart';
 
 void main() async {
